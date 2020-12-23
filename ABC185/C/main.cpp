4384478dffca0333a9f9518fa5389effaa23e26c
kogaeta <kogai@neont.net>
2020-12-23 11:34:20 +0900

    201223

--- ABC185/C/main.cpp
+++ ABC185/C/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int main(){
-    return 0;
+    int L;
+    cin >> L;
+    unsigned long long res = 1;
+    for (int i = 1; i < 12; i++)
+    {
+        res = res * (L - i) / i;
+    }
+    cout << res << endl;
 }
