3db63c151c72e4beb07cb069da17135d2c8c78d2
kogaeta <kogai@neont.net>
2020-12-24 23:16:26 +0900

    201224

--- ABC185/D/main.cpp
+++ ABC185/D/main.cpp
@@ -4,6 +4,57 @@
 
 using namespace std;
 
-int main(){
-    return 0;
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    if (M != 0)
+    {
+        vector<int> a(M);
+        vector<int> b;
+        for (int i = 0; i < M; i++)
+        {
+            cin >> a.at(i);
+        }
+        sort(a.begin(), a.end());
+
+        if (a[0] != 1 && a[0] != 2)
+        {
+            b.push_back(a[0] - 1);
+        }
+
+        for (int j = 1; j < M; j++)
+        {
+            int t = a[j] - a[j - 1] - 1;
+            if (t != 0)
+            {
+                b.push_back(t);
+            }
+        }
+
+        if (N - a[M - 1] != 0 && N - a[M - 1] != 1)
+        {
+            b.push_back(N - a[M - 1]);
+        }
+
+        if (b.empty())
+        {
+            cout << 0 << endl;
+        }
+
+        else
+        {
+            int min = *min_element(b.begin(), b.end());
+            int count = 0;
+            for (int i = 0; i < b.size(); i++)
+            {
+                count += (b[i] + (min - 1)) / min;
+            }
+            cout << count << endl;
+        }
+    }
+    else
+    {
+        cout << 1 << endl;
+    }
 }
--- ABC185/E/main.cpp
+++ ABC185/E/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int main(){
-    return 0;
+    int N, M;
+    vector<int> A(M), B(M);
+    cin >> N >> M;
+    for (int i = 0; i < M; i++){
+        cin >> A.at(i);
+    }
+    for (int i = 0; i < M; i++){
+        cin >> B.at(i);
+    }
+    
 }
